2c4f7d8de37c29b5d2bdccd323dae31b2970c606
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-04-03 07:22:52 +0530

    Add a block of code for a special case to avoid ai lose

--- Unbeatable TicTacToe/Tic_Tac_Toe.py
+++ Unbeatable TicTacToe/Tic_Tac_Toe.py
@@ -1,9 +1,9 @@
 import random
 import sys
-from logo import logo
+import logo as l
 import os
 
-def clear_console():
+def clear_console():        # For clear the consol
     # For Windows
     if os.name == 'nt':
         _ = os.system('cls')
@@ -11,11 +11,13 @@ def clear_console():
     else:
         _ = os.system('clear')
 
-def start():
-    print("Welcome to the unbeatable Tic Tac Toe")
-    print("The board's index values are: ")
-    print(logo)
-    # print("0|1|2")
+def start():            #  To start the program
+    clear_console()
+    # print("Welcome to the unbeatable Tic Tac Toe")
+    # print("The board's index values are: ")
+    print(l.logo)
+    print(l.instruction)
+    # print("0|1|2")                # These lines now moved in the logo.py file
     # print("3|4|5")
     # print("6|7|8")
     # print("You are X's and AI is O's ")
@@ -26,10 +28,12 @@ def start():
         end()
     else:
         print("Invalid input")
+        start()
 
 board = ["_","_","_","_","_","_"," "," "," "] #Board horizontal and vertical lines
 pairs = ([0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]) #Win Pairs
 corner = [0,2,6,8] #corner is used for the AI to let it pick random corner.
+sides = [1,3,5,7] # sides of the board
 
 turn = "PLAYER"
 
@@ -37,7 +41,8 @@ aiturn = 0 #number of turn of ai
 
 def print_board(turn, board, aiturn): #to print the board and start the next turn
     clear_console()
-    print(logo)
+    print(l.logo)
+    print(l.instruction)
     print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
@@ -67,17 +72,20 @@ def player_move(turn, board, aiturn):
     turn = "AI" # change turn to AI, now its ai_move
     check_win(turn, board, aiturn)
 
+result = ""
+
 def check_win(turn, board, aiturn):
+    global result
     for n in pairs: 
         first = board[n[0]]
         second = board[n[1]]
         third = board[n[2]]
         if first == second and second == third: #Check XXX and OOO are formed or not
             if first == "O":
-                print("You lose! AI win")
+                result = "You lose! AI win"
                 end() #To stop the game
             if first == "X": # This instruction will never run because this program is write for unbeatable Tic Tac Toe
-                print("You win!")
+                result = "You win!"
                 end() #To stop the game
         else:
             filled_space =0
@@ -85,14 +93,16 @@ def check_win(turn, board, aiturn):
                 if board[x] != " " and board[x] != "_":
                     filled_space +=1
                 if filled_space == 8:
-                    print("It's draw! You can't win.")
+                    result = "It's draw! You can't win."
                     end() #To stop the game
     #If game is not completed yet, then move to the next turn by calling print_board method
     print_board(turn, board, aiturn)
 
 def end(): #To end the game and print final board
     clear_console()
-    print(logo)
+    print(l.logo)
+    print(l.instruction)
+    print(result)
     print ("Thank you! Here is the final board.")
     print (board[0] + "|" + board[1] + "|" + board[2])
     print (board[3] + "|" + board[4] + "|" + board[5])
@@ -102,13 +112,27 @@ def end(): #To end the game and print final board
 def ai_move(turn, board, aiturn, corner):
     already_moved = False
 
-    def corner_choice(corner, board, already_moved):
+    def corner_choice(corner, board, already_moved):        #For corner choice [0, 2, 6, 8]
         best_choices = []
         if not already_moved:
             for n in corner:
                 if board[n] == " " or board[n] == "_":
                     best_choices.append(n)
-            board[random.choice(best_choices)] = "O"
+            if best_choices == []:
+                side_move(sides, board, already_moved)
+            else:
+                board[random.choice(best_choices)] = "O"
+
+    def side_move(sides, board, already_moved):         #Add side_move method for side move sides are [1, 3, 5, 7]
+        best_choices = []
+        if not already_moved:
+            for n in sides:
+                if board[n] == " " or board[n] == "_":
+                    best_choices.append(n)
+            if best_choices == []:
+                corner_choice(corner, board, already_moved)
+            else:
+                board[random.choice(best_choices)] = "O"
 
     if aiturn == 1: # If it is a first move of ai
         if board[4] != "X": #to check whether that middle space is already moved by the player or not if not that taken by the ai, or if taken by the player then ai have to move corner move
@@ -148,13 +172,26 @@ def ai_move(turn, board, aiturn, corner):
                     break
 
     if not already_moved:
-        if aiturn == 2 and board[4] == "X":
+        player_side_moved = []
+        for n in sides:
+            if board[n] == "X":
+                player_side_moved.append(n)
+        if aiturn == 2 and len(player_side_moved) == 2:             #I add this if statement because here is one special case where ai will lose.
+            if board[7] == "X" and board[5] == "X":                 # A|_|_
+                board[2] = "O"                                      # _|A|P
+            elif board[7] == "X"   and board[3] == "X":             #  |P| 
+                board[0] = "O"                                      # To prevent this condition occured
+            elif board[1] == "X" and board[5] == "X":
+                board[0] = "O"
+            elif board[1] == "X" and board[3] == "X":
+                board[6] = "O"
+        elif aiturn == 2 and board[4] == "X":
             corner_choice(corner, board, already_moved)
         else:
-            # P|_|_ <-- where P: Player and A: AI
-            # _|A|_  Else instruction is for this type of condition
-            #  | |P
-            sides = [1,3,5,7]
+            # _|P|P         <-- where P: Player and A: AI
+            # P|A|_         Else instruction is for check side spaces then move side_move method
+            # A| | 
+            # sides = [1,3,5,7]         # moved to the global
             player_sides = 0
             for n in sides:
                 if board[n] == "X":
@@ -162,16 +199,17 @@ def ai_move(turn, board, aiturn, corner):
             if player_sides >= 1:
                 corner_choice(corner, board, already_moved)
             else:
-                best_choices = []
-                for n in sides:
-                    best_choices.append(n)
-                if best_choices == []:
-                    corner_choice(corner, board, already_moved)
-                else:
-                    board[random.choice(best_choices)] = "O"
+                side_move(sides, board, already_moved)
+                # best_choices = []                     #  lines 182 to 188 moved to the side_move method
+                # for n in sides:
+                #     best_choices.append(n)
+                # if best_choices == []:
+                #     corner_choice(corner, board, already_moved)
+                # else:
+                #     board[random.choice(best_choices)] = "O"
 
     turn = "PLAYER"
     check_win(turn , board, aiturn)
 
-# print_board(turn, board, aiturn)
+# print_board(turn, board, aiturn)              # Update a start method in the program, now program start with the start method
 start()
