834083a4e13d638c40090522edaffcfa0ade1f03
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-04-09 15:25:34 +0530

    add some constraint

--- Guess_A_Number/Guess_a_number.py
+++ Guess_A_Number/Guess_a_number.py
@@ -29,43 +29,49 @@ def start():
 
 # easy = 10
 def easy():
-    remian = 10
+    remain = 10
     # global easy
-    for i in range(10):
-        print(f"You have {int(remian)} attempts remaining to guess the number.")
-        guess = int(input("Make a guess : "))
-        if guess == number:
-            print(f"You got it! The answer was {number}")
-            break
-        elif guess < number:
-            print("Too low! guess again.")
-            remian -= 1
-            continue
-        elif guess > number:
-            print("Too high! guess again.")
-            remian -= 1
-            continue
-    if remian == 0:
+    while remain != 0:
+        print(f"You have {int(remain)} attempts remaining to guess the number.")
+        guess = input("Make a guess : ")
+        if guess.isdigit() == False:
+            print("Input should be an integer")
+        else:
+            if int(guess) == number:
+                print(f"You got it! The answer was {number}")
+                break
+            elif int(guess) < number:
+                print("Too low! guess again.")
+                remain -= 1
+                continue
+            elif int(guess) > number:
+                print("Too high! guess again.")
+                remain -= 1
+                continue
+    if remain == 0:
         print(f"The number was {number}")
 
 # hard = 5
 def hard():
-    remian = 5
-    for i in range(5):
-        print(f"You have {remian} attempts remaining to guess the number.")
-        guess = int(input("Make a guess : "))
-        if guess == number:
-            print(f"You got it! The answer was {number}")
-            break
-        elif guess < number:
-            print("Too low! guess again.")
-            remian -= 1
-            continue
-        elif guess > number:
-            print("Too high! guess again.")
-            remian -= 1
-            continue
-    if remian == 0:
+    remain = 5
+    while remain != 0:
+        print(f"You have {remain} attempts remaining to guess the number.")
+        guess = input("Make a guess : ")
+        if guess.isdigit() == False:
+            print("Input should be an integer")
+        else:
+            if int(guess) == number:
+                print(f"You got it! The answer was {number}")
+                break
+            elif int(guess) < number:
+                print("Too low! guess again.")
+                remain -= 1
+                continue
+            elif int(guess) > number:
+                print("Too high! guess again.")
+                remain -= 1
+                continue
+    if remain == 0:
         print(f"The number was {number}")
 
 start()
